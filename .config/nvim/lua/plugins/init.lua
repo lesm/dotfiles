6c558cc3aa0d08e59500584b79f1d100b471944a
Luis Silva <silmar.dll@gmail.com>
2025-07-17 22:49:15 -0600

    task: remove tagalong.vim plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,11 +1,5 @@
 return {
 	{ "NMAC427/guess-indent.nvim", opts = {} }, -- Detect tabstop and shiftwidth automatically
-	{
-		"AndrewRadev/tagalong.vim",
-		config = function()
-			vim.g.tagalong_mappings = { "c", "C", "i", "a" }
-		end,
-	},
 	"github/copilot.vim",
 	"tpope/vim-obsession",
 	"tpope/vim-fugitive",
